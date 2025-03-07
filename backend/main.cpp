f5434e571aa87c123dbf0b0415c52a294b737cc7
Ethan Niser <ethan@niser.com>
2025-03-07 13:55:22 -0500

    backend works?

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,11 +1,13 @@
-#include <iostream>
-#include <unistd.h>
+#include "crow_all.h"
 
 int main()
 {
-    while (true)
-    {
-        std::cout << "Hello World" << std::endl;
-        sleep(1);
-    }
+    crow::SimpleApp app; // define your crow application
+
+    // define your endpoint at the root directory
+    CROW_ROUTE(app, "/")([]()
+                         { return "Hello world"; });
+
+    // set the port, set the app to run on multiple threads, and run the app
+    app.port(8080).concurrency(4).run();
 }
